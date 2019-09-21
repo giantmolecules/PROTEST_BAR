6c5949b46c6592a8ef27f9726babe922272f0e87
Brett Ian Balogh <brettbalogh@gmail.com>
2019-09-21 16:41:09 -0400

    reorganizing

---
+++ FIRMWARE/OLD/PROTEST-BAR/PROTEST-BAR.ino
@@ -0,0 +1,222 @@
+// ===== Settings ===== //
+const uint8_t channels[] = {1, 6, 11}; // used Wi-Fi channels (available: 1-14)
+const bool wpa2 = true; // WPA2 networks
+const bool appendSpaces = true; // makes all SSIDs 32 characters long to improve performance
+const uint8_t numSpaces = 3;
+/*
+  SSIDs:
+  - don't forget the \n at the end of each SSID!
+  - max. 32 characters per SSID
+  - don't add duplicates! You have to change one character at least
+*/
+const char ssids[] PROGMEM = {
+"!a   I can't believe\n"
+"!b   It's come to\n"
+"!c   The point where\n"
+"!d   I have to reach\n"
+"!e   Out to you here\n"
+};
+
+char prefixes[]={'!','#','$'};
+
+#include "WiFi.h"
+
+extern "C" {
+#include "esp_wifi.h"
+  esp_err_t esp_wifi_set_channel(uint8_t primary, wifi_second_chan_t second);
+  esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
+}
+
+// run-time variables
+char emptySSID[32];
+uint8_t channelIndex = 0;
+uint8_t macAddr[6];
+uint8_t wifi_channel = 1;
+uint32_t currentTime = 0;
+uint32_t packetSize = 0;
+uint32_t packetCounter = 0;
+uint32_t attackTime = 0;
+uint32_t packetRateTime = 0;
+
+// beacon frame definition
+uint8_t beaconPacket[109] = {
+  /*  0 - 3  */ 0x80, 0x00, 0x00, 0x00, // Type/Subtype: managment beacon frame
+  /*  4 - 9  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Destination: broadcast
+  /* 10 - 15 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // Source
+  /* 16 - 21 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // Source
+
+  // Fixed parameters
+  /* 22 - 23 */ 0x00, 0x00, // Fragment & sequence number (will be done by the SDK)
+  /* 24 - 31 */ 0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00, // Timestamp
+  /* 32 - 33 */ 0xe8, 0x03, // Interval: 0x64, 0x00 => every 100ms - 0xe8, 0x03 => every 1s
+  /* 34 - 35 */ 0x31, 0x00, // capabilities Tnformation
+
+  // Tagged parameters
+
+  // SSID parameters
+  /* 36 - 37 */ 0x00, 0x20, // Tag: Set SSID length, Tag length: 32
+  /* 38 - 69 */ 0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20, // SSID
+
+  // Supported Rates
+  /* 70 - 71 */ 0x01, 0x08, // Tag: Supported Rates, Tag length: 8
+  /* 72 */ 0x82, // 1(B)
+  /* 73 */ 0x84, // 2(B)
+  /* 74 */ 0x8b, // 5.5(B)
+  /* 75 */ 0x96, // 11(B)
+  /* 76 */ 0x24, // 18
+  /* 77 */ 0x30, // 24
+  /* 78 */ 0x48, // 36
+  /* 79 */ 0x6c, // 54
+
+  // Current Channel
+  /* 80 - 81 */ 0x03, 0x01, // Channel set, length
+  /* 82 */      0x01,       // Current Channel
+
+  // RSN information
+  /*  83 -  84 */ 0x30, 0x18,
+  /*  85 -  86 */ 0x01, 0x00,
+  /*  87 -  90 */ 0x00, 0x0f, 0xac, 0x02,
+  /*  91 -  92 */ 0x02, 0x00,
+  /*  93 - 100 */ 0x00, 0x0f, 0xac, 0x04, 0x00, 0x0f, 0xac, 0x04, /*Fix: changed 0x02(TKIP) to 0x04(CCMP) is default. WPA2 with TKIP not supported by many devices*/
+  /* 101 - 102 */ 0x01, 0x00,
+  /* 103 - 106 */ 0x00, 0x0f, 0xac, 0x02,
+  /* 107 - 108 */ 0x00, 0x00
+};
+
+// goes to next channel
+void nextChannel() {
+  if (sizeof(channels) > 1) {
+    uint8_t ch = channels[channelIndex];
+    channelIndex++;
+    if (channelIndex > sizeof(channels)) channelIndex = 0;
+
+    if (ch != wifi_channel && ch >= 1 && ch <= 14) {
+      wifi_channel = ch;
+      //wifi_set_channel(wifi_channel);
+      esp_wifi_set_channel(wifi_channel, WIFI_SECOND_CHAN_NONE);
+    }
+  }
+}
+
+// generates random MAC
+void randomMac() {
+  for (int i = 0; i < 6; i++)
+    macAddr[i] = random(256);
+}
+
+void setup() {
+  pinMode(13, OUTPUT);
+  // create empty SSID
+  for(int i = 0; i < 32; i++){
+    emptySSID[i]=' ';
+  }
+  // for random generator
+  randomSeed(1);
+
+  // set packetSize
+  packetSize = sizeof(beaconPacket);
+  if (wpa2) {
+    beaconPacket[34] = 0x31;
+  } else {
+    beaconPacket[34] = 0x21;
+    packetSize -= 26;
+  }
+
+  // generate random mac address
+  randomMac();
+  
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  //say hi
+  Serial.println(">>>>>>> PROTEST BAR <<<<<<<");
+  //change WiFi mode
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  // set channel
+  esp_wifi_set_channel(channels[0], WIFI_SECOND_CHAN_NONE);
+  //esp_wifi_set_channel(6, WIFI_SECOND_CHAN_NONE);
+
+  // print out saved SSIDs
+  Serial.println("SSIDs:");
+  int i = 0;
+  int len = sizeof(ssids);
+  while(i < len){
+    Serial.print((char)pgm_read_byte(ssids + i));
+    i++;
+  }
+}
+
+void loop() {
+  digitalWrite(13, HIGH);
+  // put your main code here, to run repeatedly:
+  currentTime = millis();
+
+  // send out SSIDs
+  if (currentTime - attackTime > 1000) {
+    attackTime = currentTime;
+
+    // temp variables
+    int i = 0;
+    int j = 0;
+    int ssidNum = 1;
+    char tmp;
+    int ssidsLen = strlen_P(ssids);
+    bool sent = false;
+
+    // go to next channel
+    nextChannel();
+
+    while (i < ssidsLen) {
+      // read out next SSID
+      j = 0;
+      do {
+        tmp = pgm_read_byte(ssids + i + j);
+        j++;
+      } while (tmp != '\n' && j <= 32 && i + j < ssidsLen);
+
+      uint8_t ssidLen = j - 1;
+
+      // set MAC address
+      macAddr[5] = ssidNum;
+      ssidNum++;
+
+      // write MAC address into beacon frame
+      memcpy(&beaconPacket[10], macAddr, 6);
+      memcpy(&beaconPacket[16], macAddr, 6);
+      // reset SSID
+      memcpy(&beaconPacket[38], emptySSID, 32);
+      Serial.println("memcpy");
+      // write new SSID into beacon frame
+      //memcpy_P(&beaconPacket[38+numSpaces+1], &ssids[i], ssidLen);
+      memcpy_P(&beaconPacket[38], &ssids[i], ssidLen);
+      // set channel for beacon frame
+      beaconPacket[82] = wifi_channel;
+
+      // send packet
+      if (appendSpaces) {
+        for (int k = 0; k < 3; k++) {
+          packetCounter += esp_wifi_80211_tx(ESP_IF_WIFI_STA, beaconPacket, packetSize, 0) == 0;
+          delay(1);
+        }
+      }
+      
+      i += j;
+    }
+  }
+
+  // show packet-rate each second
+  if (currentTime - packetRateTime > 1000) {
+    packetRateTime = currentTime;
+    Serial.print("Packets/s: ");
+    Serial.println(packetCounter);
+    packetCounter = 0;
+  }
+}
+
